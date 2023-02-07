46002216660efb368eb01137eb81ab5d2109fdd6
zacmonroe <zmonroe0820@gmail.com>
2023-02-07 14:43:47 -0600

    added blog home page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { MainWithCenteredContent } from "../../components";
+
+const BlogHome: React.FC = () => {
+	return <MainWithCenteredContent>Blog home</MainWithCenteredContent>;
+};
+
+export default BlogHome;
