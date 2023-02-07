d7ac27d6b396065a89cce6240412d1c2711cd434
zacmonroe <zmonroe0820@gmail.com>
2023-02-07 14:43:27 -0600

    added simple styled `main` component

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+// @index('./*', f => `export * from '${f.path}'`)
+export * from "./main-with-centered-content";
+// @endindex
---
+++ src/components/main-with-centered-content.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainWithCenteredContent = styled.main`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	height: 100%;
+`;
