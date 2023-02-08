55eb207e3d9da202156c10e068377b82dd4dff2e
zacmonroe <zmonroe0820@gmail.com>
2023-02-08 17:40:15 -0600

    add blog page template

---
+++ src/components/page-templates/blog-page-template.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from "react";
+import styled from "styled-components";
+
+const FullHeightMain = styled.main`
+	height: 100%;
+	padding-block: 4em;
+	padding-inline: 10em;
+`;
+
+const Header = styled.h1``;
+
+const Subheader = styled.p`
+	margin-bottom: 2em;
+	font-weight: normal;
+`;
+
+export interface BlogPageTemplateProps {
+	dateCreated: string;
+	dateModified?: string;
+	title: string;
+}
+
+// TODO Dynamically define page's title with React Helmet (or similar)
+export const BlogPageTemplate: React.FC<
+	PropsWithChildren<BlogPageTemplateProps>
+> = ({ children, ...props }) => {
+	return (
+		<FullHeightMain>
+			<Header>{props.title}</Header>
+			<Subheader>
+				<i>Published on {props.dateModified ?? props.dateCreated}</i>
+			</Subheader>
+
+			{children}
+		</FullHeightMain>
+	);
+};
