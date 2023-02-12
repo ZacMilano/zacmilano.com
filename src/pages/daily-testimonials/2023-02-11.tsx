24aadaf2ac164c55f439a3579532d18c629aa0d5
zacmonroe <zmonroe0820@gmail.com>
2023-02-11 21:20:58 -0600

    added today's testimonial from Lorem's Ipsum

---
+++ src/pages/daily-testimonials/2023-02-11.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import styled from "styled-components";
+
+import {
+	TwoColumnTestimonial,
+	TestimonialPageTemplate,
+} from "../../components";
+
+const BlurbColumn = styled.div``;
+
+const CardWithDecor = styled.article`
+	position: relative;
+
+	width: 30em;
+
+	padding: 3em;
+	text-align: justify;
+	text-indent: 3em;
+
+	background-color: black;
+	color: white;
+	border-radius: 1em;
+	box-shadow: 0em 0.5em 5em hsla(0, 0%, 0%, 0.3);
+
+	&::before,
+	&::after {
+		content: "";
+		position: absolute;
+		z-index: -1;
+
+		width: 20%;
+		height: 50%;
+
+		border-radius: inherit;
+		box-shadow: 0em 0.5em 5em hsla(0, 0%, 0%, 0.3);
+	}
+
+	&::before {
+		transform: rotate(-15deg);
+
+		top: -1em;
+		left: -1.5em;
+
+		background-color: cornflowerblue;
+	}
+
+	&::after {
+		transform: rotate(15deg);
+
+		bottom: -1em;
+		right: -1.5em;
+
+		background-color: deepskyblue;
+	}
+`;
+
+const QuotedPerson = styled.p`
+	text-align: right;
+`;
+
+const Testimonial: React.FC = () => {
+	return (
+		<CardWithDecor>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+				quasi mollitia obcaecati ullam repellendus. Error, non officiis?
+				Nesciunt iste nam, distinctio eos nihil delectus, vero necessitatibus
+				ducimus quae iure illo quasi autem ullam eligendi modi suscipit esse
+				voluptate in asperiores? Quae amet hic impedit delectus mollitia aut
+				eaque officia magnam.
+			</p>
+
+			<QuotedPerson>&mdash; Allison Yoyd</QuotedPerson>
+		</CardWithDecor>
+	);
+};
+
+// TODO: Define consistent structure to automatically include each day in the
+// index page for this meta-project, and to not repeat myself
+const businessName = "Lorem's Ipsum";
+
+const TodaysTestimonialPage: React.FC = () => {
+	return (
+		<TestimonialPageTemplate date="2023-02-10" businessName={businessName}>
+			<TwoColumnTestimonial>
+				<Testimonial />
+				<BlurbColumn>
+					<p>
+						I don't have much time today, so I'm just doing something simple
+						real quick.
+					</p>
+
+					<p>
+						Today's business is Lorem's Ipsum again, specializing in non-english
+						copy text.
+					</p>
+				</BlurbColumn>
+			</TwoColumnTestimonial>
+		</TestimonialPageTemplate>
+	);
+};
+
+export default TodaysTestimonialPage;
+
+export const Head: React.FC = () => {
+	return (
+		<>
+			<title>{businessName} | Testimonials</title>
+			<script
+				src="https://kit.fontawesome.com/6ab573e32e.js"
+				crossOrigin="anonymous"
+			/>
+		</>
+	);
+};
--- src/pages/daily-testimonials/index.tsx
+++ src/pages/daily-testimonials/index.tsx
@@ -10,6 +10,7 @@ const TestimonialsHome: React.FC = () => {
 				2023-02-09: Froggy's Daycare
 			</InternalLink>
 			<InternalLink to="./2023-02-10">2023-02-10: Lorem's Ipsum</InternalLink>
+			<InternalLink to="./2023-02-11">2023-02-11: Lorem's Ipsum</InternalLink>
 		</MainWithCenteredContent>
 	);
 };
