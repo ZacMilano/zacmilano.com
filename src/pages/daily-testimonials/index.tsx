134352d25a42b07b45b0af5cb4ab6eaa9f7b94e9
zacmonroe <zmonroe0820@gmail.com>
2023-02-16 10:55:37 -0600

    simplified daily testimonial home navigation

--- src/pages/daily-testimonials/index.tsx
+++ src/pages/daily-testimonials/index.tsx
@@ -6,24 +6,33 @@ import {
 	NavigationHeader,
 } from "../../components";
 
+interface TestimonialPracticeConfig {
+	date: string;
+	businessName: string;
+}
+
+const testimonialPractices: TestimonialPracticeConfig[] = [
+	{ date: "2023-02-08", businessName: "FaaSt Foods" },
+	{ date: "2023-02-09", businessName: "Froggy's Daycare" },
+	{ date: "2023-02-10", businessName: "Lorem's Ipsum" },
+	{ date: "2023-02-11", businessName: "Lorem's Ipsum" },
+	{ date: "2023-02-12", businessName: "Superb Bowls" },
+	{ date: "2023-02-13", businessName: "Chunky Dunkies" },
+	{ date: "2023-02-14", businessName: "Lorem's Ipsum" },
+	{ date: "2023-02-15", businessName: "Lorem's Ipsum" },
+];
+
 const TestimonialsHome: React.FC = () => {
 	return (
 		<>
 			<NavigationHeader />
 
 			<MainWithCenteredContent>
-				<InternalLink to="./2023-02-08">2023-02-08: FaaSt Foods</InternalLink>
-				<InternalLink to="./2023-02-09">
-					2023-02-09: Froggy's Daycare
-				</InternalLink>
-				<InternalLink to="./2023-02-10">2023-02-10: Lorem's Ipsum</InternalLink>
-				<InternalLink to="./2023-02-11">2023-02-11: Lorem's Ipsum</InternalLink>
-				<InternalLink to="./2023-02-12">2023-02-12: Superb Bowls</InternalLink>
-				<InternalLink to="./2023-02-13">
-					2023-02-13: Chunky Dunkies
-				</InternalLink>
-				<InternalLink to="./2023-02-14">2023-02-14: Lorem's Ipsum</InternalLink>
-				<InternalLink to="./2023-02-15">2023-02-15: Lorem's Ipsum</InternalLink>
+				{testimonialPractices.map(({ date, businessName }) => (
+					<InternalLink to={`./${date}`}>
+						{`${date}: ${businessName}`}
+					</InternalLink>
+				))}
 			</MainWithCenteredContent>
 		</>
 	);
